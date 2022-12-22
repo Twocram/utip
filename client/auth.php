2cb0652d959fa067c795df3d6e620b61aed4f3c5
Twocram <2002maslovartem@gmail.com>
2022-12-22 12:08:20 +0500

    removed comments

--- client/auth.php
+++ client/auth.php
@@ -48,30 +48,6 @@ if (isset($_SESSION['auth']['user'])) {
             </div>
 
         </div>
-        <!--         
-        <div class="form-container">
-
-            <div class="slider"></div>
-            <div class="btn">
-                <button class="login">Login</button>
-                <button class="signup">Signup</button>
-            </div>
-
-            <div class="form-section">
-
-                <div class="login-box">
-                    <input type="email" id="email-login" class="email ele" placeholder="youremail@email.com">
-                    <input type="password" id="password-login" class="password ele" placeholder="password">
-                    <button id="login-btn" class="clkbtn">Login</button>
-                </div>
-
-                <div class="login-box">
-                    <input type="email" id="email-reg" class="email ele" placeholder="youremail@email.com">
-                    <input type="password" id="password-reg" class="password ele" placeholder="password">
-                    <button id="reg-btn" class="clkbtn">Signup</button>
-                </div>
-            </div>
-        </div> -->
     </div>
 
 
